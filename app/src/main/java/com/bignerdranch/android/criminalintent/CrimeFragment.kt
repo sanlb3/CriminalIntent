e6aae75b2bba288c4f58a9d5ce217abb2e5b0fa2
sanlb3 <sanlb2810@gmail.com>
2022-11-22 21:18:57 -0800

    Chapter 13

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -16,8 +16,11 @@ import java.util.*
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
+private const val DIALOG_DATE = "DialogDate"
+private const val REQUEST_DATE = 0
 
-class CrimeFragment : Fragment(){
+
+class CrimeFragment : Fragment(), DatePickerFragment.Callbacks{
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -46,10 +49,6 @@ class CrimeFragment : Fragment(){
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
 
         return view
     }
@@ -95,6 +94,13 @@ class CrimeFragment : Fragment(){
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
             }
+
+            dateButton.setOnClickListener {
+                DatePickerFragment.newInstance(crime.date).apply {
+                    setTargetFragment(this@CrimeFragment, REQUEST_DATE)
+                    show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+                }
+            }
         }
 
     }
@@ -102,6 +108,11 @@ class CrimeFragment : Fragment(){
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDateSelected(date: Date) {
+        crime.date = date
+        updateUI()
     }
 
     private fun updateUI() {
